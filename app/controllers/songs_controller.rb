30ee3476f8288be3e1d27886fd60008423294c27
nicksolony <nicksolony@gmail.com>
2020-03-12 01:38:49 -0400

    created new create form with flash message

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,32 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  use Rack::Flash
+  
   get '/songs' do
     @songs = Song.all
     erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    @genres = Genre.all.sort_by(&:name)
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+
+    @song = Song.create(name: params[:song_name])
+
+    if params[:artist_name] != ""
+      @song.artist= Artist.create(name: params[:artist_name])
+    end
+    params[:genres].each do |genre_id|
+      SongGenre.create(song_id: @song.id, genre_id: genre_id.to_i)
+    end
+  #  binding.pry
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug' do
